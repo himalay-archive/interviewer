88c50561fcd73d7c98f192b2789425881f7aaf21
Himalay <contact@himalay.com.np>
2016-08-07 13:15:01 +0545

    Add a sample question and display on ion-slide

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -1,11 +1,27 @@
 import {Component} from '@angular/core';
 import {NavController} from 'ionic-angular';
 
+import {StorageProvider, Question} from '../../providers/storage/storage';
+
 @Component({
   templateUrl: 'build/pages/home/home.html'
 })
 export class HomePage {
-  constructor(public navCtrl: NavController) {
+  questions: Array<Question> = [];
 
+  constructor(public navCtrl: NavController, public  storage : StorageProvider) {
+    let sampleQuestion = new Question('What is the question?', null);
+    this.storage.create(sampleQuestion).then(() => console.log('question added'), err => console.error(err));
+  }
+
+  private loadQuestions() {
+    this.storage.get()
+    .then((questions: Array<Question>) => {
+      this.questions = questions;
+    });
+  }
+
+  ionViewDidEnter () {
+    this.loadQuestions();
   }
 }
