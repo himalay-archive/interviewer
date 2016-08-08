3f42982031419dd08ab737abb55f061fbdaaaabe
Himalay <contact@himalay.com.np>
2016-08-08 18:28:58 +0545

    Check if candidate is set

--- app/app.ts
+++ app/app.ts
@@ -11,13 +11,16 @@ import {WelcomePage} from './pages/welcome/welcome';
   template: '<ion-nav [root]="rootPage"></ion-nav>'
 })
 export class MyApp {
-  rootPage: any = HomePage;
+  rootPage: any;// = WelcomePage;
 
-  constructor(platform: Platform) {
+  constructor(platform: Platform, storage: StorageProvider) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       StatusBar.styleDefault();
+      storage.getKV('candidate').then(candidate => {
+        this.rootPage = candidate ? HomePage : WelcomePage;
+      })
     });
   }
 }
